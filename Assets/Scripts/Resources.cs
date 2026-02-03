eab5c0131c0357d38dab5afe2cf360d106b5cabd
David McFarlane <d-mcfarlane@live.com>
2026-02-03 00:59:35 -0400

    Add a few more easing functions and tweak ease distance

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -61,9 +61,41 @@ public static class Utils
         eases.Add(EaseInOutBack);
         eases.Add(EaseInOutCubic);
         eases.Add(EaseInOutCirc);
+        eases.Add(EaseInOutBounce);
+        eases.Add(EaseOutBounce);
         return eases[Random.Range(0, eases.Count - 1)];
     }
 
+    public static float EaseOutBounce(float x)
+    {
+        var n1 = 7.5625f;
+        var d1 = 2.75f;
+
+        if (x < 1 / d1)
+        {
+            return n1 * x * x;
+        }
+
+        if (x < 2 / d1)
+        {
+            return n1 * (x -= 1.5f / d1) * x + 0.75f;
+        }
+
+        if (x < 2.5 / d1)
+        {
+            return n1 * (x -= 2.25f / d1) * x + 0.9375f;
+        }
+
+        return n1 * (x -= 2.625f / d1) * x + 0.984375f;
+    }
+
+    public static float EaseInOutBounce(float x)
+    {
+        return x < 0.5
+            ? (1 - EaseOutBounce(1 - 2 * x)) / 2
+            : (1 + EaseOutBounce(2 * x - 1)) / 2;
+    }
+    
     public static float EaseInOutCirc(float x)
     {
         return x < 0.5
